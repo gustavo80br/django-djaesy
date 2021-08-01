ce424bfe2f9b9dfa388afe658d2dd1e1421b676d
Gustavo Gonçalves <gustavo@fundacaoaprender.org.br>
2021-08-01 16:21:33 -0300

    setup scritp

---
+++ djaesy/management/commands/djaesy_admin.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+import argparse
+import os
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+djaesy_package = 'git+https://github.com/gustavo80br/django-djaesy'
+src_folder = 'src'
+
+
+def start_project(project_name, execution_path):
+    project_path = os.path.join(execution_path, project_name)
+    if not Path(os.path.join(execution_path, project_name)).is_dir():
+        os.mkdir(project_path)
+        os.chdir(project_path)
+        subprocess.call([sys.executable, '-m', 'pip', 'install', 'django'], shell=True)
+        subprocess.call([sys.executable, '-m', 'pip', 'install', 'virtualenv'], shell=True)
+        subprocess.call(['django-admin', 'startproject', project_name], shell=True)
+        os.rename(project_name, src_folder)
+        os.chdir('..')
+    else:
+        print("Assuming the project exists, skipping.")
+
+
+def install_virtualenv(project_path, execution_path):
+    if not Path(os.path.join(project_path, 'venv')).is_dir():
+        os.chdir(project_path)
+        subprocess.call(['virtualenv', '--python', sys.executable, 'venv'], shell=True)
+        os.chdir(execution_path)
+    else:
+        print("Assuming the virtualenv exists, skipping.")
+
+
+def create_gitignore(project_path):
+    if not Path(os.path.join(project_path, '.gitignore')).exists():
+        gitignore = open(f"{project_path}/.gitignore", "w")
+        gitignore.write("""
+venv/
+.idea/
+__pycache__/
+*.py[cod]
+*$py.class
+        """)
+        gitignore.close()
+    else:
+        print("Assuming the .gitignore exists, skipping.")
+
+
+def call_in_venv_step(project, project_path, src_path, main_path):
+    venv_python_bin = os.path.join(project_path, 'venv', 'Scripts', 'python.exe')
+    subprocess.call([venv_python_bin,
+        'bootstrap-djaesy.py',
+        project,
+        '--path', project_path,
+        '--src-folder', src_path,
+        '--main-folder', main_path,
+        '--in-venv'
+    ])
+
+
+def append_item_to_list(position_re, append_str, work_file, adjust=2):
+
+    regexp = re.compile(position_re)
+
+    fp = open(work_file, "r")
+    file_str = fp.read()
+
+    match_str = regexp.findall(file_str)
+
+    if match_str:
+        match_str = match_str[0]
+    else:
+        return
+
+    match_pos = file_str.find(match_str)
+    match_pos = match_pos + len(match_str) - adjust
+
+    file_head = file_str[:match_pos]
+    file_bottom = file_str[match_pos:]
+
+    file_str = file_head + append_str + file_bottom
+
+    fp.close()
+    fp = open(work_file, "w")
+    fp.write(file_str)
+    fp.close()
+
+
+def install_djaesy(project_path):
+    if not Path(os.path.join(project_path, 'requirements.txt')).exists():
+        subprocess.call(['pip', 'install', '--no-cache-dir', '-U', djaesy_package], shell=True)
+        requirements = open(os.path.join(project_path, 'requirements.txt'), "w")
+        requirements.write(djaesy_package)
+        requirements.close()
+    else:
+        print(f'Assuming requirements.txt exists, skipping. Add {djaesy_package} to the requirements.txt manually')
+
+
+def setup_djaesy(project_path, src_folder, main_folder):
+    append_item_to_list(r"INSTALLED_APPS = \[[\s'\w\.,]*\]", "\n    'djaesy',", f"{os.path.join(project_path, src_folder, main_folder, 'settings.py')}")
+    append_item_to_list(r"from django\.urls import path", ", include", f"{os.path.join(project_path, src_folder, main_folder, 'urls.py')}", adjust=0)
+    append_item_to_list(r"urlpatterns = \[[\s\w\('\/,\.\)]*\]", "\n    path('', include('djaesy.urls')),", f"{os.path.join(project_path, src_folder, main_folder, 'urls.py')}")
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("project")
+    parser.add_argument("--in-venv", action='store_true')
+    parser.add_argument("--path")
+    parser.add_argument("--src-folder")
+    parser.add_argument("--main-folder")
+
+    arguments, other_arguments = parser.parse_known_args()
+    return arguments, other_arguments
+
+
+if __name__ == '__main__':
+
+    args, other_args = parse_arguments()
+    project = args.project
+
+    start_pwd = os.getcwd()
+    project_pwd = os.path.join(start_pwd, project)
+
+    if not args.in_venv:
+        print("Starting Django project")
+        start_project(project, start_pwd)
+        print("Installing a virtualenv environment")
+        install_virtualenv(project_pwd, start_pwd)
+        print("Creating a .gitignore file")
+        create_gitignore(project_pwd)
+        print("Going into Venv environment...")
+        call_in_venv_step(project, project_pwd, src_folder, project)
+    else:
+        path = args.path
+        src_path = args.src_folder
+        main_path = args.main_folder
+        print("Installing Djaesy into venv")
+        install_djaesy(path)
+        print("Setting up Djaesy for the project")
+        setup_djaesy(path, src_path, main_path)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import find_packages, setup
+from setuptools import setup
 
 # with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 #     README = readme.read()
@@ -9,58 +9,4 @@ from setuptools import find_packages, setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup(
-    name='django-djaesy',
-    version='0.1.0',
-    packages=['djaesy'],
-    package_data={'djaesy': ['*']},
-    include_package_data=True,
-    license='MIT License',
-    description='Django Djaesy',
-    long_description='Django Djaesy',
-    url='https://github.com/gustavo80br/django-djaesy/',
-    author='Gustavo H. de A. Gonçalves',
-    author_email='gustavo@gemtech.net.br',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 3.0.1',  # replace "X.Y" as appropriate
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
-    install_requires=[
-        'Django==3.2.5',
-        'django-localflavor==3.1',
-        'django-autocomplete-light==3.8.2',
-        'django-crum==0.7.9',
-        'django-crispy-forms==1.12.0',
-        'django-filter==2.4.0',
-        'django-image-cropping==1.5.0',
-        'django_countries==7.2.1',
-        'django_polymorphic==3.0.0',
-        'django-stubs==1.8.0',
-        'django_leaflet==0.28.1',
-        'djangorestframework==3.12.4',
-        'djangorestframework-stubs==1.4.0',
-        'django-cleanup==5.2.0',
-        'simplejson==3.17.3',
-        'orjson==3.6.0',
-        'django-geojson==3.2.0',
-        'django-chartjs==2.2.1',
-        'pandas==1.3.1',
-        'jinja2==3.0.1',
-        'openpyxl==3.0.7',
-        'xlwt==1.3.0',
-        'xlsxwriter==1.4.5',
-        'easy_thumbnails==2.7.1',
-        'requests==2.26.0',
-        'pytz==2021.1',
-        'whitenoise[brotli]==5.3.0',
-        'markdown==3.3.4',
-    ]
-)
+setup()
